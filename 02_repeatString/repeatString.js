62507d85c9bd4d8af71431cb42d8fb188cd1ccf0
Edson Petry <edsonlbpetry@gmail.com>
2025-01-08 16:59:37 -0500

    completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    try {
+        return string.repeat(n);
+    } catch (error) {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
