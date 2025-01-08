d942218ba12c6ccfbe78ec7561a218053ec9c8ab
Edson Petry <edsonlbpetry@gmail.com>
2025-01-08 17:10:56 -0500

    completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    reversedArray = [];
 
+    for (i = string.length; i >= 0; i--){
+        reversedArray.push(string.charAt(i));
+    }
+    
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
