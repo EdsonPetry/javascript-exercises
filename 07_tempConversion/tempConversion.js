155da2d3165f508cf1ab79186d533a86df3fa866
Edson Petry <edsonlbpetry@gmail.com>
2025-01-08 20:30:46 -0500

    completed 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // Formula (x-32) * (5/9) as per https://en.wikipedia.org/wiki/Conversion_of_scales_of_temperature
+  if (fahrenheit === 32){
+    return 0;
+  }
+  else {
+    return parseFloat(((fahrenheit-32)*(5/9)).toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  if (celsius === 0){
+    return 32;
+  } else {
+    return parseFloat(((celsius*(1.8))+32).toFixed(1));
+  }
 };
 
 // Do not edit below this line
