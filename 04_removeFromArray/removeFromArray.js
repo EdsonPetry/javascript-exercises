741b9a86c08dfbbc7668606c7a5ffdb2316d42da
Edson Petry <edsonlbpetry@gmail.com>
2025-01-08 19:00:14 -0500

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
 const removeFromArray = function() {
+    // INIT args array SET to Arrays.from(arguments)
+    const args = Array.from(arguments);
+    // INIT input array SET to args[0]
+    const input = args[0];
+    // INIT toRemove array SET to the remaining arguments in args
+    const toRemove = args.splice(1);
+    // INIT filtered SET to empty array
+    let filtered = []
+    // FOR EACH loop to push elements that are NOT in toRemove
+    input.forEach(element => {
+        if (!(toRemove.includes(element))){
+            filtered.push(element);
+        }
+    });
+
+    return filtered;
+    
 };
 
 // Do not edit below this line
