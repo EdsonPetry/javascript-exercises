0079021dc699269f00a2caa06fc5d723a300efee
Edson Petry <edsonlbpetry@gmail.com>
2025-01-08 20:04:54 -0500

    completed 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const condition1 = (year % 4 == 0) && !(year % 100 == 0);
+    const condition2 = (year % 100 == 0) && (year % 400 == 0);
+    
+    if (condition1){
+        return true;
+    } else if (condition2) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
