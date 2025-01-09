1a4b8d9b7ad12a2e2758f6605c6cdaf121f378df
Edson Petry <edsonlbpetry@gmail.com>
2025-01-08 19:31:48 -0500

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let total = 0;
+
+    // Check if num1 and num2 are numbers
+    if (!(Number.isInteger(num1)) || !(Number.isInteger(num2))) {
+        return "ERROR";
+    }
+
+    // Check if num1 and num2 are nonnegative
+    if ((num1 < 0) || (num2 < 0)) {
+        return "ERROR";
+    }
+    const min = Math.min(num1, num2);
+    const max = Math.max(num1, num2);
+    for (i = min; i <= max; i++) {
+        total += i;
+    }
+
+    return total;
 
 };
 
