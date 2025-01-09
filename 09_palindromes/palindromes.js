c929000e355fc891579751534e12194de38b1fbc
Edson Petry <edsonlbpetry@gmail.com>
2025-01-08 21:17:03 -0500

    completed 09_palindaroms

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    strArray = str.toLowerCase().split("");
+    letters = strArray.filter(isLetter);
+    reversed_letters = letters.toReversed();
+
+    return letters.join("") === reversed_letters.join("");
 
 };
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]|[0-9]/i);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
